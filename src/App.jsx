48ab1355b5aabe63a50f8c390f9cb4eb8d0f3380
Nusiek <norbert.furmanik@op.pl>
2025-02-14 02:10:19 +0100

    setup + components

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from './components/Hero'
+import Generator from './components/Generator'
+import Workout from './components/Workout'
+
+const App = () => {
+  return (
+    <>
+      <Hero />
+      <Generator />
+      <Workout />
+    </>
+  )
+}
+
+export default App
